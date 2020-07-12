790e125089b238840039caffd6d4c558b5c940eb
Robert Bergers <bergersr97@gmail.com>
2020-07-12 10:00:08 -0400

    Design DesktopNavigation component

--- src/react/components/ui/DesktopNavigation.js
+++ src/react/components/ui/DesktopNavigation.js
@@ -13,17 +13,43 @@ export class DesktopNavigation extends React.Component {
     render() {
         return (
             <section id="DesktopNavigation">
-                <ul className="DesktopNavigation__list">
-                    {this.props.internal_links &&
-                        this.props.internal_links.map((link) => (
-                            <li key={link.text}>
-                                <a href={link.href}>
-                                    {link.text}
-                                </a>
-                            </li>
-                        ))
-                    }
-                </ul>
+                <nav className="DesktopNavigation__nav">
+                    <ul className="DesktopNavigation__list">
+                        {/* NavLinks */}
+                        {this.props.navlinks &&
+                            this.props.navlinks.map((link) => (
+                                <li 
+                                    key={link.text}
+                                    className="DesktopNavigation__link"
+                                >
+                                    <NavLink
+                                        to={link.route}
+                                    >
+                                        {link.text}
+                                    </NavLink>
+                                </li>
+                            ))
+                        }
+
+                        {/* External links */}
+                        {this.props.links &&
+                            this.props.links.map((link) => (
+                                <li 
+                                    key={link.text}
+                                    className="DesktopNavigation__link"
+                                >
+                                    <a 
+                                        href={link.href} 
+                                        target={link.external && "_blank"}
+                                        rel="noopener"
+                                    >
+                                        {link.text}
+                                    </a>
+                                </li>
+                            ))
+                        }                    
+                    </ul>
+                </nav>
             </section>
         );
     };
